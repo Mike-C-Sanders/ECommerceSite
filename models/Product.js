59ff7ad62eebb678424f79cdb4ed91b0abf3db1d
Mike Sanders <msanders331@gmail.com>
2022-03-20 17:09:34 -0700

    Created the associations to models in index file

--- models/Product.js
+++ models/Product.js
@@ -37,7 +37,7 @@ Product.init(
     category_id:{
       type: DataTypes.INTEGER,
       references: {
-        modelName: 'category',
+        model: 'category',
         key: 'id',
       }
     }
