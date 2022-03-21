01fc28f49e647b9877a983566754d21de9cd66bc
Mike Sanders <msanders331@gmail.com>
2022-03-20 17:18:29 -0700

    added sequelize to the server file and synced it to the port.

--- models/index.js
+++ models/index.js
@@ -15,11 +15,13 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 module.exports = {
   Product,
